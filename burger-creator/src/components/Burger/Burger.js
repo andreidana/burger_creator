2d9fe73f5810b6980ea54918b9754ef2b256ded8
Andrei Dana <andrei.dana87@gmail.com>
2019-06-03 23:49:20 +0300

    Create burger controls

--- burger-creator/src/components/Burger/Burger.js
+++ burger-creator/src/components/Burger/Burger.js
@@ -4,12 +4,19 @@ import BurgerIngredient from './BurgerIngredient/BurgerIngredient';
 import './Burger.css';
 
 const burger = (props) => {
+  let ingredients = Object.keys(props.ingredients)
+    .map(ingredient => [...Array(props.ingredients[ingredient])].map((item, index) => <BurgerIngredient key={ingredient + index} type={ingredient}/>))
+    .reduce((arr, el) => arr.concat(el), []);
+
+  if (ingredients.length === 0) {
+    ingredients = <p>Please start adding ingredients</p>
+  }
+
   return (
     <div className="Burger">
-      <BurgerIngredient type="bread-top" />
-      <BurgerIngredient type="cheese" />
-      <BurgerIngredient type="meat" />
-      <BurgerIngredient type="bread-bottom" />
+      <BurgerIngredient type="bread-top"/>
+      {ingredients}
+      <BurgerIngredient type="bread-bottom"/>
     </div>
   );
 }
